0448aa00942b8d8ec948574ec51829e7ea3eadec
Muhammad Hafiz Hasan <mhafizhasan@gmail.com>
2022-07-05 11:42:19 +0800

    codemr

---
+++ codemr/spring-petclinic/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,17 @@
+var matrix = [[0,0,0,0,2],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,4],[0,0,0,0,0]]
+var packages = [{
+"name": " org.springframework.samples.petclinic.vet", "color": " #3182bd"
+}
+,{
+"name": " org.springframework.samples.petclinic.system", "color": " #6baed6"
+}
+,{
+"name": " org.springframework.samples.petclinic", "color": " #9ecae1"
+}
+,{
+"name": " org.springframework.samples.petclinic.owner", "color": " #c6dbef"
+}
+,{
+"name": " org.springframework.samples.petclinic.model", "color": " #e6550d"
+}
+];
